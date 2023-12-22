69ab30864951f912e4a9c843feeb7d84c7b1ea21
bindujakkamsetti <121660576+bindujakkamsetti@users.noreply.github.com>
2023-12-22 20:58:23 +0530

    web_functions.py

--- web_functions.py
+++ web_functions.py
@@ -24,7 +24,7 @@ def load_data():
 def train_model(X, y):
     """This function trains the model and return the model and model score"""
     # Create the model
-    model = andomForestClassifier(
+    model = RandomForestClassifier(
             ccp_alpha=0.0, class_weight=None, criterion='entropy',
             max_depth=4, max_features=None, max_leaf_nodes=None,
             min_impurity_decrease=0.0, min_samples_leaf=1, 
